d682984d67062998b57cd2843f152a69aedb10ad
Kian Cross <kian@kiancross.co.uk>
2020-12-23 01:28:06 +0000

    Updated types (#8)

--- Makefile
+++ Makefile
@@ -8,6 +8,7 @@ help:
 	@echo "    test           Run unit tests"
 	@echo "    style-check    Check code style using black"
 	@echo "    fix-fix        Fix any code styling issues using black"
+	@echo "    types          Check types"
 
 .PHONY: test
 test:
@@ -20,3 +21,7 @@ style-check:
 .PHONY: style-fix
 style-fix:
 	black .
+
+.PHONY: types
+types:
+	mypy --strict pixy
--- pixy/extended_colour.py
+++ pixy/extended_colour.py
@@ -2,12 +2,13 @@
 # Copyright (c) 2020 Kian Cross
 #
 
+from typing import cast
 from pixy.escape_sequence import EscapeSequence
 from pixy.colours import constants
 
 
 class ExtendedColour(EscapeSequence):
-    def __new__(self, colour_code: int, background: bool = False):
+    def __new__(cls, colour_code: int, background: bool = False) -> "ExtendedColour":
 
         if colour_code < 0 or colour_code > 255:
             raise ValueError("colour_code must be in-between 0 and 255")
@@ -17,4 +18,4 @@ class ExtendedColour(EscapeSequence):
         else:
             type_code = constants.FOREGROUND_CODE
 
-        return super().__new__(self, type_code, 5, colour_code)
+        return cast(ExtendedColour, super().__new__(cls, type_code, 5, colour_code))
